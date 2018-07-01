2618044e28d1df220af4d7e840d99b894912e776
glatan <glatan.edu@gmail.com>
2018-07-01 13:45:29 +0900

    トイレSOS

---
+++ Tech Shot/Assets/Scripts/FallCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour {
+
+    private GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+    //更新
+    void Update () {
+        cube.transform.position = new Vector3(1, 0, 0);
+    }
+}
