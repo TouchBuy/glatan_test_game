ab85f325715b616f41be2481560471d7d353264a
glatan <glatan.edu@gmail.com>
2018-07-01 17:06:08 +0900

    トリガーを押すと(0,1,1)に球体が出てくるようになった

---
+++ Tech Shot/Assets/Scripts/FallenSphere.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenSphere : MonoBehaviour {
+
+    public GameObject sphere;
+
+    //更新
+    void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) {
+            Instantiate(sphere);
+        }
+    }
+}
