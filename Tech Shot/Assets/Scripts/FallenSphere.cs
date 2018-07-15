fb4026002b64b5fc927b4f5c257ec81b6bb9162b
glatan <glatan.edu@gmail.com>
2018-07-15 09:15:01 +0900

    bit.csをいったん削除

--- Tech Shot/Assets/Scripts/FallenSphere.cs
+++ Tech Shot/Assets/Scripts/FallenSphere.cs
@@ -9,10 +9,7 @@ public class FallenSphere : MonoBehaviour {
     //更新
     void Update () {
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) {
-            for (int i=0; i<10; i++)
-            {
-                Instantiate(sphere);
-            }
+            Instantiate(sphere);
         }
     }
 }
